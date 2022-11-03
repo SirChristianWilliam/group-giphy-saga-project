c7be74b4a88574a51261f59f879dd2dc736fd067
Sam Phipps <samuel.v.phipps@gmail.com>
2022-11-03 13:12:11 -0500

    fixed bug

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,12 @@
 import React, {useState, useEffect} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import Header from '../../../server/modules/Header/Header';
+import Header from '../Header/Header';
 // import './App.css';
 
 function App(props) {
   return (
     <div>
-      <h1>Giphy Search!</h1>
+      <Header />
     </div>
   );
 }
