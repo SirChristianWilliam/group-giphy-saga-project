e57caeb2d08998e2d50ec846ccb83fda1b17eb2b
Sam Phipps <samuel.v.phipps@gmail.com>
2022-11-03 13:07:03 -0500

    header done

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import Header from '../../../server/modules/Header/Header';
 // import './App.css';
 
 function App(props) {
