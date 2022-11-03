b85e008bc3e35434727b3d93696babec5273b602
Chris Maki <c.william.developer@gmail.com>
2022-11-03 10:36:22 -0500

    Skeleton for index.js set up, database skeleton set up

--- src/index.js
+++ src/index.js
@@ -1,5 +1,68 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+// Provider allows us to use redux within our react app
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+// Import saga middleware
+import createSagaMiddleware from 'redux-saga';
+import axios from 'axios';
+import { takeEvery, put } from 'redux-saga/effects';
+// import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// const search = (state = [], action) => {
+//     console.log('in search reducer', state);
+// }
+
+// const favorites = (state = [], action) => {
+//     console.log('in favorites reducer', state)
+// }
+
+// function* fetchSearch() {
+//     console.log('in fetchSearch');
+// }
+
+// function* fetchFavorites() {
+//     console.log('in fetchFavorites');
+// }
+
+// function* addFavorites() {
+//     console.log('in addFavorites');
+// }
+
+// function* deleteFavorites() {
+//     console.log('in deleteFavorites');
+// }
+
+// function* changeCategory() {
+//     console.log('in changeCategory');
+// }
+
+
+function* watcherSaga() {
+
+    yield takeEvery('FETCH_SEARCH');
+
+    yield takeEvery('FETCH_FAVORITES');
+
+    yield takeEvery('ADD_FAVORITE');
+
+    yield takeEvery('DELETE_FAVORITE');
+
+    yield takeEvery('CHANGE_CATEGORY');
+}
+
+const sagaMiddleware = createSagaMiddleware();
+
+const storeInstance = createStore(
+    combineReducers({
+        // search,
+        // favorites,
+    }),
+    applyMiddleware(sagaMiddleware,logger),
+);
+
+sagaMiddleware.run(watcherSaga);
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, 
+document.getElementById('root'));
